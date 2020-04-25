1f6be2bb1c4ba0dbc2ed111083e9db751b320923
zach-nelson <49630043+zen123555@users.noreply.github.com>
2020-04-25 11:04:04 -0700

    Add files via upload

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,40 @@
+import anvil.email
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+from datetime import datetime
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def add_feedback(name, email, feedback):
+  app_tables.feedback.add_row(
+    name=name,
+    email=email,
+    feedback=feedback,
+    created=datetime.now() 
+  )
+  # Sends an email each time feedback is submitted
+  anvil.email.send(to="nelsonz@cwu.edu", # Change this to your email address!
+                   subject=f"Feedback from {name}",
+                   text=f"""
+  A new person has filled out the feedback form!
+
+  Name: {name}
+  Email address: {email}
+  Feedback:
+  {feedback}
+  """)
+
+
